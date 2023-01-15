e2d67d5a9b01b4d71d40b931bbffe9c3e14d9447
tai <mizuno.kagawa@outlook.jp>
2023-01-15 22:55:40 +0900

    javascript

---
+++ script.js
@@ -0,0 +1,69 @@
+//変数
+let str = "Hello World";
+// console.log(str);
+// // str = "こんにちは！";
+// console.log(str);
+
+//定数
+const str2 = "おはよう";
+// console.log(str2);
+
+//配列
+let array = ["いち", "にい", "さん", "よん"];
+// console.log(array[2]);
+
+//ループ文
+// let index = 0;
+// while (index < array.length) {
+//     console.log(array[index]);
+//     index++;
+// }
+
+//if else
+// if (array.length > 5) {
+//     console.log("おはよう");
+// } else {
+//     console.log("さよなら");
+// }
+
+//関数
+const test = (arg) => {
+    if (array.length > arg) {
+        console.log("おはよう");
+    } else {
+        console.log("さよなら");
+    }
+}
+
+//オブジェクト
+const test2 = {
+    color: "red",
+    size: "small",
+    lebel: 5,
+    sample: () => {
+        console.log("Hello");
+    }
+};
+
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+    alert("こんにちは！！");    
+});
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+    btn.textContent = "変更";
+    btn.style.color = "black";
+    btn.style.fontWeight = "bold";
+    btn.style.background = "red";
+});
+
+const btn2 = document.getElementById("btn2");
+const body = document.body;
+btn2.addEventListener("click", () => {
+    body.style.backgroundColor = "#f0f0f0";
+});
+
+const btn3 = document.getElementsByTagName("button")[3];
+btn3.addEventListener("click", () => {
+    btn3.remove();
+})
